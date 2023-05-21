08ef30405a27781fed4b7cd731473bb0ce8c3ac5
rashmiranjan9178 <125286238+rashmiranjan9178@users.noreply.github.com>
2023-05-21 15:16:54 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science ToolsAnd Ecosystem
+
+# ## In  this notebook, Data science Tools and Ecosystem are summerised
+
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. julia
+# 5. java
+# 6. C++
+
+# 1. NumPy
+# 2. Pandas
+# 3. Matplotlib
+# 4. Scikit-learn
+# 5. TensorFlow
+# 6. scipy
+# 7. ggplot
+# 8. seaborn
+# 9. pytorch
+
+# | Data Science Tools |
+# | ------ |
+# | Jupyter Notebook |
+# | RStudio |
+# | Spyder |
+# |SQL database|
+# |Apache Hadoop|
+# |Apache Spark|
+# |Tableau|
+
+# In[3]:
+
+
+##Below are a few example of evaluating arithmatic expressions in python
+# Addition
+a = 10
+b = 5
+addition = a + b
+print("Addition:", addition)
+# Subtraction
+subtraction = a - b
+print("Subtraction:", subtraction)
+# Multiplication
+multiplication = a * b
+print("Multiplication:", multiplication)
+# Division
+division = a / b
+print("Division:", division)
+# Floor Division
+floor_division = a // b
+print("Floor Division:", floor_division)
+
+# Modulo
+modulo = a % b
+print("Modulo:", modulo)
+
+# Exponentiation
+exponentiation = a ** b
+print("Exponentiation:", exponentiation)
+
+
+# In[4]:
+
+
+#This a simple arithmatic expression to multiply then add integers
+a=10
+b=20
+c=30
+print((a*b)+c)
+
+
+# In[5]:
+
+
+Time_in_min=200
+Time_in_Hour=Time_in_min/60
+print(Time_in_Hour)
+
+
+# | Objectives |
+# | ------ |
+# | libraries for ds |
+# | Tools for ds |
+# | Arithmatic operation |
+# | Addition and Multiplication operation |
+# | Usesof Tags and comment lines |
+
+# 
+
+# ## Author
+# . Rasmiranjan panda
+# . From india
+# . Student of GIFT
+
+# In[ ]:
+
+
+
+
